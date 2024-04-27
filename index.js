bcf6a3b8f0ea32aa0f04f432e4d9e09bf5d7398a
morshidulrahman <boydanger416@gmail.com>
2024-04-27 10:33:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+//midleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
